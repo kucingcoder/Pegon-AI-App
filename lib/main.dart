eb17a2e66fca7e70b5435b71c283c53051c9371a
M. Ibrahim Hanif <137396727+kucingcoder@users.noreply.github.com>
2026-01-19 22:14:19 +0700

    Legal brand

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Pegon AI',
+      title: 'Pegon AI : Membaca, Menulis, dan Belajar',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFFFFD700)),
         useMaterial3: true,
